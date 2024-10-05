c4cb0b72e572cb35c8d1d6b16e0d23859260a240
Christian Toney <christian@makuwro.com>
2024-10-04 22:43:36 -0400

    Add video array

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./HomePage.module.css";
 import tiktokVideo from "./tiktok-video.mp4";
+import rotationRally from "./rotation-rally.mp4";
 
 export default function HomePage() {
+
+  const videos = [tiktokVideo, rotationRally];
+  const [videoIndex, setVideoIndex] = useState(0);
 
   document.title = "College Democrats at Central Michigan University";
 
@@ -11,7 +15,7 @@ export default function HomePage() {
       <section id={styles.videoContainer}>
         <h1>We are the College Democrats at Central Michigan University</h1>
         <p>We dedicate ourselves to organizing students through issue education, advocacy, voter registration, and turnout as we strive to create a more just society for everyone.</p>
-        <video muted src={tiktokVideo} autoPlay loop style={{
+        <video muted src={videos[videoIndex]} onEnded={() => setVideoIndex(videos[videoIndex + 1] ? videoIndex + 1 : 0)} autoPlay style={{
           position: "absolute",
           zIndex: 0
         }}/>
