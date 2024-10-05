499e80b023339a655fad93e2496ba2fafb4b3846
Christian Toney <toney1c@cmich.edu>
2024-10-05 11:53:26 -0400

    Fix header (#43)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 import logoImage from "./logo.png"
 
 export default function Header() {
 
+  const [shouldShowBackground, setShouldShowBackground] = useState<boolean>(false);
+
+  useEffect(() => {
+
+    function checkScrollPosition() {
+
+      setShouldShowBackground(window.scrollY >= 633);
+      
+    }
+
+    document.addEventListener("scroll", checkScrollPosition);
+
+    return () => {
+
+      document.removeEventListener("scroll", checkScrollPosition);
+
+    }
+
+  }, []);
+
   return (
-    <header id={styles.header}>
+    <header id={styles.header} className={shouldShowBackground ? styles.background : undefined}>
       <section id={styles.left}>
         <Link to="/">
           <img src={logoImage} />
