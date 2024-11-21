2e3064bc2520a9b108332460814fc358fe3bb107
Christian Toney <christian@makuwro.com>
2024-11-21 12:52:12 -0500

    Use blame instead of commits by default

--- src/pages/ConstitutionPage/ConstitutionPage.tsx
+++ src/pages/ConstitutionPage/ConstitutionPage.tsx
@@ -16,7 +16,7 @@ export default function ConstitutionPage() {
         <Markdown remarkPlugins={[sectionize]}>{constitutionText}</Markdown>
       </section>
       <section id={styles.actionList}>
-        <button onClick={() => location.href = "https://github.com/cmichdemocrats/website/commits/production/src/pages/ConstitutionPage/constitution.md"}>View history</button>
+        <button onClick={() => location.href = "https://github.com/cmichdemocrats/website/blame/production/src/pages/ConstitutionPage/constitution.md"}>View history</button>
         <button onClick={() => location.href = "https://github.com/cmichdemocrats/website/edit/production/src/pages/ConstitutionPage/constitution.md"}>Propose changes</button>
       </section>
     </main>
